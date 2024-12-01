f1b347d2b27807a294759caaad97b1abdb052867
AlexCristea04 <alexcristea004@gmail.com>
2024-12-01 13:50:11 -0500

    Models

---
+++ FinalProject/Models/Exhibition.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Data;
+
+public class Exhibition
+{
+    [Key]
+    public int exhibitionId { get; set; } 
+
+    [Required]
+    [StringLength(100)]
+    public string title { get; set; } 
+
+    [Required]
+    public string description { get; set; } 
+
+    [Required]
+    public int artistId { get; set; }
+
+    public Artist artist { get; set; } 
+
+    public ICollection<Painting> paintings { get; set; } 
+}
---
+++ FinalProject/Models/Painting.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Data;
+
+public class Painting
+{
+    [Key]
+    public int paintingId { get; set; } 
+
+    [Required]
+    [StringLength(100)]
+    public string title { get; set; } 
+
+    [Required]
+    [StringLength(100)]
+    public string artist { get; set; } 
+
+    [Required]
+    public string medium { get; set; } 
+
+    [Required]
+    public string dimensions { get; set; } 
+    
+    [Required]
+    [DataType(DataType.Currency)]
+    public decimal price { get; set; } 
+
+    public string story { get; set; } 
+
+    [Required]
+    [StringLength(255)]
+    public string imageUrl { get; set; } 
+
+    [Required]
+    public int exhibitionId { get; set; } 
+
+    public Exhibition exhibition { get; set; } 
+
+    public int rating { get; set; } 
+}
