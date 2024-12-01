7970886f2370f8db94cd174d7b80cac51b997f5b
AlexCristea04 <alexcristea004@gmail.com>
2024-12-01 15:47:50 -0500

    html base

---
+++ FinalProject/Pages/Cart.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject.Pages;
+
+public class Cart : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -64,5 +65,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapRazorPages();
 
 app.Run();
